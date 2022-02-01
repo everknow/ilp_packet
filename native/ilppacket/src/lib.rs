da0a6975db58aa8814d4887c0559fcef9e82d207
Miquel <miquel.garcia@betika.com>
2022-02-01 16:27:25 +0100

    Added tests and some cleanup

--- native/ilppacket/src/lib.rs
+++ native/ilppacket/src/lib.rs
@@ -3,7 +3,7 @@ use rustler::types::binary::{Binary};
 use bytes::{BytesMut};
 
 use rustler::types::atom::{ok, error};
-use rustler::{Encoder, Env, Term, NifResult};
+use rustler::{Atom, Encoder, Env, Term, NifResult};
 use std::convert::TryFrom;
 use crate::custom_atoms::{fulfill, prepare, reject};
 
@@ -18,37 +18,18 @@ mod custom_atoms {
 #[rustler::nif(schedule = "DirtyCpu")]
 fn decode<'a>(env: Env<'a>, bin: Binary) -> NifResult<Term<'a>> {
     match Packet::try_from(BytesMut::from(bin.as_slice())) {
-        Ok(Packet::Prepare(p)) => {
-            Term::map_from_arrays(env, &[
-                "type",
-                "data"
-            ].map( |x| x.encode(env)),&[
-                prepare().encode(env),
-                p.data().encode(env)
-            ])
-        }
-        Ok(Packet::Fulfill(f)) => {
-            Term::map_from_arrays(env, &[
-                "type",
-                "data"
-            ].map( |x| x.encode(env)),&[
-                fulfill().encode(env),
-                f.data().encode(env)
-            ])
-        }
-        Ok(Packet::Reject(r)) => {
-            Term::map_from_arrays(env, &[
-                "type",
-                "data"
-            ].map( |x| x.encode(env)),&[
-                reject().encode(env),
-                r.data().encode(env)
-            ])
-        }
-        Err(_) => {
-            Ok((error()).encode(env))
-        }
+        Ok(Packet::Prepare(p)) => response(env, prepare(), p.data()),
+        Ok(Packet::Fulfill(p)) => response(env, fulfill(), p.data()),
+        Ok(Packet::Reject(p)) => response(env, reject(), p.data()),
+        Err(err) => Ok((error(), err.to_string()).encode(env))
     }
+}
+
+fn response<'a>(env: Env<'a>, packet_type: Atom, data: &[u8]) -> NifResult<Term<'a>> {
+    let keys = ["type", "data"];
+    let values = [packet_type.encode(env), data.encode(env)];
+    let result = Term::map_from_arrays(env, &keys.map( |x| x.encode(env)), &values);
+    Ok((ok(), result.unwrap()).encode(env))
 }
 
 rustler::init!("Elixir.IlpPacket", [decode]);
